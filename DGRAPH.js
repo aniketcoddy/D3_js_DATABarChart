ba9d74da5303079c4ac0619ed17b934e124450d4
Aniket <vishnoianiket4@gmail.com>
2023-06-14 23:46:37 +0530

    final commit

--- DGRAPH.js
+++ DGRAPH.js
@@ -1,10 +1,10 @@
 const data = [
-    { name: "simran", score: 23 },
-    { name: "Aniket", score: 90 },
-    { name: "abhisekh", score: 34 },
-    { name: "divya", score: 78 },
-    { name: "kikola", score: 98 },
-    { name: "deepali", score: 100 },
+    { name: "Simran", score: 23 },
+    { name: "Aniket", score: 87 },
+    { name: "Abhisekh", score: 34 },
+    { name: "Divya", score: 78 },
+    { name: "Krishna", score: 45 },
+    { name: "Deepali", score: 100 },
 ]
 
 var width = 800
@@ -19,7 +19,7 @@ const svg = d3.select("#d3-container").append("svg")
 const x = d3.scaleBand()
 .domain(d3.range(data.length))
 .range([margin.left,width-margin.right])
-.padding(0.1);
+.padding(0.5)
 
 const y = d3.scaleLinear()
 .domain([0,100])
@@ -27,7 +27,7 @@ const y = d3.scaleLinear()
 
 svg
 .append('g')
-.attr('fill','royalblue')
+.attr('fill','#67cadf')
 .selectAll('rect')
 .data(data.sort((a,b)=>d3.descending(a.score,b.score)))
 .join('rect')
@@ -35,17 +35,17 @@ svg
 .attr('y',(d)=>y(d.score))
 .attr('height',d=>y(0)-y(d.score))
 .attr('width',x.bandwidth())
+.attr('class','rectangle')
+
 
 function xAxis(g){
     g.attr('transform',`translate(0,${height-margin.bottom})`)
     .call(d3.axisBottom(x).tickFormat(i=>data[i].name))
-    .attr('font-size','20px')
 }
 
 function yAxis(g){
     g.attr('transform',`translate(${margin.left},0)`)
     .call(d3.axisLeft(y).ticks(null,data.format))
-    .attr('font-size','20px')
 }
 
 svg.append('g').call(xAxis);
